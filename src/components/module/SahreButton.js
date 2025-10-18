1bc50c9186f6240f696e3e104ccc84cc537241ed
daniyal <daniyal.madise@gmail.com>
2025-10-18 16:37:05 +0330

    main3

--- src/components/module/SahreButton.js
+++ src/components/module/SahreButton.js
@@ -1,7 +1,6 @@
 "use client";
 import styles from "@/module/ShareButton.module.css";
 import { useEffect, useState } from "react";
-import { CopyToClipboard } from "react-copy-to-clipboard";
 import toast, { Toaster } from "react-hot-toast";
 import { LuShare2 } from "react-icons/lu";
 
